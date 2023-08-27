48e1477ae2815d4908366e5d679b40e9aac002fe
sarawaniolka <sara.waniolka@gmail.com>
2023-08-28 00:18:55 +0200

    small change

--- src/main.jl
+++ src/main.jl
@@ -13,13 +13,13 @@ using .onepix_mod
 using .CW_mod
 
 # Load the image
-image_path = "masters_thesis/original_photo.jpg"
+image_path = "masters_thesis/original_photo2.jpg"
 original_image = Images.load(image_path);
 original_img = model_mod.resize_image(original_image);
 preprocessed_img = model_mod.normalize_tensor!(model_mod.preprocess_image(original_img));
 original_prediction = model_mod.predict(preprocessed_img)
 
-FGSM_mod.FGSM_attack(original_img, original_prediction[2], 0.108);
+FGSM_mod.FGSM_attack(original_img, original_prediction[2], 0.19);
 FGSM_image = Images.load("masters_thesis/FGSM_attack.jpg")
 preprocessed_FGSM = model_mod.normalize_tensor!(model_mod.preprocess_image(FGSM_image));
 FGSM_prediction = model_mod.predict(preprocessed_FGSM)
