408d9ab508cdd683c4f8ae10b0a30d2dbe2680f5
sarawaniolka <sara.waniolka@gmail.com>
2023-09-07 21:40:21 +0200

    one pixel attack added

--- src/main.jl
+++ src/main.jl
@@ -19,7 +19,7 @@ original_image = Images.load(image_path);
 # Preprocess the image
 original_img = model_mod.resize_image(original_image)
 preprocessed_image = model_mod.preprocess_image(original_img);
-save("original_image.jpg", original_img)
+save("original_image.jpg", original_img);
 # Original prediction
 original_prediction = model_mod.predict(preprocessed_image);
 println("Original prediction: ", original_prediction)
@@ -31,15 +31,15 @@ println("Prediciton after FGSM attack: ", FGSM_prediction)
 println("Chosen epsilon: ", epsilon)
 
 # CW attack
-adv = CW_mod.cw_attack(original_img, 282, 0.1, 10);
-CW_prediction = model_mod.predict(adv[1]);
+adv = CW_mod.cw_attack(original_img, 282, 0.5, 0.9, 50);
+CW_prediction = model_mod.predict(adv);
 println("Prediciton after CW attack: ", CW_prediction)
 
 # Randomized Initial Pixel: (151, 165, 3, 1)
 #Prediction: 
 #("Egyptian cat", 286)
 
-onepix_mod.one_pixel_attack(original_img);
-onepix_image = Images.load("one_pixel_attack.jpg");
-# TO DO
+onepix_image, pixels= onepix_mod.one_pixel_attack(original_img, 100, 1000);
 one_pixel_prediction = model_mod.predict(onepix_image)
+println(pixels)
+println("Prediciton after pixel attack: ", one_pixel_prediction)
--- src/one_pixel.jl
+++ src/one_pixel.jl
@@ -1,9 +1,7 @@
-# One Pixel Attack
 module onepix_mod
     using Flux
     using Images
     using Random
-    #using Optim
 
     include("model.jl")
     include("FGSM.jl")
@@ -11,24 +9,53 @@ module onepix_mod
     import .FGSM_mod
 
 
-    function perturb_image(img, pixel)
-        perturbed_image = copy(img)
-        perturbed_image[pixel...] = 1.0
-        a = reshape(perturbed_image, 3, 224, 224)
-        b = colorview(RGB, a)
-        save("one_pixel_attack.jpg", b)
+    function normalize_data(img)
+        # Scale the pixel values to the (0,1) range
+        min_val = minimum(img)
+        max_val = maximum(img)
+        pixel_data = (img .- min_val) / (max_val - min_val)
+        return pixel_data
     end
 
-    function one_pixel_attack(img)
-        width = rand(1:224)
-        height = rand(1:224)
-        channel = rand(1:3)
-        initial_pixel = (width, height, channel, 1)
-        
-        println("Randomized Initial Pixel: $initial_pixel")
-        
-        preprocessed_image = FGSM_mod.FGSM_preprocess(img)
-        perturb_image(preprocessed_image, initial_pixel)
+
+    function visualise_image(img, iteration)
+        reshaped_adv_x = permutedims(img, [3, 1, 2])
+        # Create an image from the normalized data
+        img = colorview(RGB, reshaped_adv_x)
+        save("one_pixel_attack_$iteration.jpg", img)
+    end
+
+    function one_pixel_attack(img, max_iterations, num_pixels_to_change)
+        preprocessed_image = model_mod.preprocess_image(img);
+        p = onepix_mod.normalize_data(preprocessed_image)
+        original_prediction = model_mod.predict(p);
+
+        for i in 1:max_iterations
+            pixels = select_pixels(num_pixels_to_change)
+            altered_picture = copy(p)
+
+            for pixel in pixels
+                width, height, channel = pixel
+                altered_picture[width, height, channel] = 0.0
+            end
+            adversarial_prediction = model_mod.predict(altered_picture)
+
+            if adversarial_prediction[2] != original_prediction[2] && adversarial_prediction[2] != 286
+                visualise_image(altered_picture, i)
+                println("Adversarial found in iteration ", i)
+                return altered_picture, pixels
+            else
+                println("Unable to find an adversarial attack with $num_pixels_to_change pixels")
+            end
+        end
+        return preprocessed_image
+    end
+    
+    
+
+    function select_pixels(num_pixels)
+        pixels = [(rand(1:224), rand(1:224), rand(1:3)) for _ in 1:num_pixels]
+        return pixels
     end
     
 end
